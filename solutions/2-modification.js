5a6db8cef14951d51faa5df918b6650dcd00d1e6
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:30:56 +0300

    Finished 2 task.

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,3 +1,14 @@
 // BEGIN
 
+export const swap =(arr)=>{
+    if (arr.length === 0) return arr;
+    if (arr.length === 1) return arr;
+    else {
+        let t = arr[0];
+        arr[0] = arr[arr.length-1];
+        arr[arr.length-1] = t;
+        return arr;
+    }
+}
+export default swap;
 // END
