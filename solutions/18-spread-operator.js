3dd2ebadc6ac9a7d688354b065ffec9fa6b83595
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:59:43 +0300

    Finished 18 task.

--- solutions/18-spread-operator.js
+++ solutions/18-spread-operator.js
@@ -1,3 +1,14 @@
 // BEGIN
-
+export const flatten =(arr)=>{
+    const res = [];
+    for(const temp of arr){
+        if(Array.isArray(temp)){
+            res.push(...temp);
+        }else {
+            res.push(temp);
+        }
+    }
+    return res;
+}
+export default flatten;
 // END
