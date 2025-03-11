3a8f41cf9bc99f9a741755041bbec068e7c1732c
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:44 +0300

    Finished 10 task.

--- solutions/10-nested-arrays.js
+++ solutions/10-nested-arrays.js
@@ -1,3 +1,22 @@
 // BEGIN
-
+export const getSuperSeriesWinner=(scores)=>{
+    let canada =0;
+    let ussr =0;
+    for(const score of scores){
+        if(score[0]>score[1]){
+            canada++;
+        }else if(score[0]<score[1]){
+            ussr++;
+        }
+    }
+    if(canada>ussr) {
+        return "canada";
+    }
+    else if(canada<ussr) {
+        return "ussr";
+    } else {
+        return null;
+    }
+}
+export default getSuperSeriesWinner;
 // END
