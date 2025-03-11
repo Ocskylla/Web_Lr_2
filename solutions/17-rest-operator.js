e7f8a6c68f2783fdf52aba81a9d2c196f01002e0
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:54:30 +0300

    Finished 17 task.

--- solutions/17-rest-operator.js
+++ solutions/17-rest-operator.js
@@ -1,3 +1,16 @@
 // BEGIN
-
+export const getMax=(arr)=>{
+    if(arr.length === 0){
+        return null;
+    }
+    const [first,...rest] = arr;
+    let max = first;
+    for(const n of rest){
+        if(n > max){
+            max = n;
+        }
+    }
+    return max;
+}
+export default getMax;
 // END
