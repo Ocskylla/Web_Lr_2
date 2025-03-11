56997bf932cd4352abb8d7e1091132e51315da16
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:35 +0300

    Finished 9 task.

--- solutions/9-control-statements.js
+++ solutions/9-control-statements.js
@@ -1,3 +1,12 @@
 // BEGIN
-
+export const getTotalAmount =(money, name)=>{
+    let total = 0;
+    for(const parity of money){
+        if(parity.slice(0,3)===name){
+            total+=Number(parity.slice(3));
+        }
+    }
+    return total;
+}
+export default getTotalAmount;
 // END
