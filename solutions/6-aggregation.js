2f709abb93b55f62e5d63df95b67e59d55b2daf2
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:21 +0300

    Finished 6 task.

--- solutions/6-aggregation.js
+++ solutions/6-aggregation.js
@@ -1,3 +1,11 @@
 // BEGIN
-
+export  const calculateSum =(arr) =>{
+    let sum = 0;
+    for (let i =0; i<arr.length; i++){
+        if(arr[i]%3===0){sum+=arr[i];
+        }
+    }
+    return sum;
+}
+export default calculateSum;
 // END
