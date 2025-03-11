b95ee6385beecf2b04872dc1c683001b21ced726
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:24 +0300

    Finished 7 task.

--- solutions/7-for-of.js
+++ solutions/7-for-of.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export const calculateAverage = (arr) =>{
+    let sum = 0;
+    let temp;
+    if (arr.length === 0) { return null;}
+    for(temp of arr){sum+=temp}
+    return sum/arr.length;
+}
+export default calculateAverage;
 // END
