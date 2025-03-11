3eee74e9141975301bb63179ba57f578ce1d625c
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:37:53 +0300

    Finished 15 task.

--- solutions/15-sorting.js
+++ solutions/15-sorting.js
@@ -1,3 +1,18 @@
 // BEGIN
-
+export const bubbleSort = (arr)=> {
+    const n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        let swapped = false;
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+    }
+    return arr;
+}
+export default bubbleSort;
 // END
