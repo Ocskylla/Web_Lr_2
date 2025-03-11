e25a0c03be7dbbd23ad8f70b763bcd76af982ca2
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:25:17 +0300

    Finished 1 task.

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,11 @@
 // BEGIN (write your solution here)
-
+export const getWeekends = (format = "long") =>{
+    if(format === 'long'){
+        return ['saturday','sunday'];
+    }
+    else if(format === 'short'){
+        return ['sat','sun'];
+    }
+}
+export default getWeekends;
 // END
