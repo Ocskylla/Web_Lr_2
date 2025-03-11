c6a590767caec391551268ac00d7d53ffe2e6653
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:30 +0300

    Finished 8 task.

--- solutions/8-removing.js
+++ solutions/8-removing.js
@@ -1,3 +1,20 @@
 // BEGIN
+export const getSameParity =(arr)=>{
+    if(arr.length===0){
+        return [];
+    }
+    const firstElement = arr[0];
+    const isFirstEven = Math.abs(firstElement) % 2 === 0;
 
+    const result = [];
+    for (const number of arr) {
+        if ((Math.abs(number) % 2 === 0) === isFirstEven) {
+            result.push(number);
+        }
+    }
+
+    return result;
+
+}
+export default getSameParity;
 // END
