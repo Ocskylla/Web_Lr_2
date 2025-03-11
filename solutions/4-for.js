66ab5f04654a979cba9b79b97aeeec3be0749da0
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:09 +0300

    Finished 4 task.

--- solutions/4-for.js
+++ solutions/4-for.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export const addPrefix = (arr, prefix) => {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(prefix +" "+ arr[i]);
+    }
+    return newArr;
+}
+export default addPrefix;
 // END
