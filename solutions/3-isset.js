b91c2b02a96449ffda61ce098ce5e5e729c6a700
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:31:02 +0300

    Finished 3 task.

--- solutions/3-isset.js
+++ solutions/3-isset.js
@@ -1,3 +1,10 @@
 // BEGIN
-
+export const get = (arr, index, def = null) => {
+    if (index >= 0 && index < arr.length) {
+        return arr[index];
+    } else {
+        return def;
+    }
+}
+export default get;
 // END
