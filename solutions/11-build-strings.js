f85514ca5120dee254874e65924ab78f1d1aeca9
Ocskylla <BAAD410@yandex.ru>
2025-03-11 10:32:15 +0300

    Finished 11 task.

--- solutions/11-build-strings.js
+++ solutions/11-build-strings.js
@@ -1,3 +1,16 @@
 // BEGIN
+export const buildDefinitionList = (definitions)=>{
+    if(definitions.length === 0 ){
+        return '';
+    }
+    let html = '<dl>';
+    for (const definition of definitions) {
+        const [term, description] = definition;
+        html += `<dt>${term}</dt><dd>${description}</dd>`;
+    }
+    html += '</dl>';
 
+    return html;
+}
+export default buildDefinitionList;
 // END
