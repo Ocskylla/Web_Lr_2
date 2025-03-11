d3c06ecc982f1e2dc7302ed0ea558b1022a7b804
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:19:38 +0300

    Finished 12 task.

--- solutions/12-strings.js
+++ solutions/12-strings.js
@@ -1,3 +1,16 @@
 // BEGIN
-
+export const makeCensored =(text, badWords)=> {
+    let words = text.split(' ');
+    let censored = [];
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        if(badWords.includes(word)) {
+            censored.push("$#%!");
+        }else {
+            censored.push(word);
+        }
+    }
+    return censored.join(' ');
+}
+export default makeCensored;
 // END
