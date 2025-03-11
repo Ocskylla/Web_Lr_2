efa36dd1cbe0c9ad671cb9aa2be2c266dd3664b2
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:24:57 +0300

    Finished 13 task.

--- solutions/13-nested-loops.js
+++ solutions/13-nested-loops.js
@@ -1,5 +1,21 @@
-const _ = require("lodash"); 
+import {uniq} from "lodash";
+
+const _ = require("lodash");
 
 // BEGIN
-
+export const getSameCount =(arr1,arr2) => {
+    let count = 0;
+    let uniqArr1 = uniq(arr1);
+    let uniqArr2 = uniq(arr2);
+    for(let i = 0; i < uniqArr1.length; i++) {
+        for(let j = 0; j < uniqArr2.length; j++) {
+            if(uniqArr1[i] === uniqArr2[j]) {
+                count++;
+                break;
+            }
+        }
+    }
+return count;
+}
+export default getSameCount;
 // END
