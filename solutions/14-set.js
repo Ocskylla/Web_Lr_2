0931a34f3f48b76e74287c89bc52b2ca62fc9760
Ocskylla <BAAD410@yandex.ru>
2025-03-11 13:33:20 +0300

    Finished 14 task.

--- solutions/14-set.js
+++ solutions/14-set.js
@@ -1,3 +1,12 @@
 // BEGIN
+import {set} from "lodash";
 
+export const countUniqChars =(string)=> {
+    const set1 = new Set();
+    for (const char of string) {
+        set1.add(char);
+    }
+    return set1.size;
+}
+export default countUniqChars;
 // END
