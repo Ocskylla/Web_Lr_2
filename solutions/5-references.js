4f010e1a8b560d45f08a5fdd53503a9c37c8658b
Ocskylla <BAAD410@yandex.ru>
2025-03-13 01:08:10 +0300

    Update 5-references.js

--- solutions/5-references.js
+++ solutions/5-references.js
@@ -1,6 +1,15 @@
 // BEGIN
-export const reverse = (arr)=>{
-    return arr.reverse()
-}
+export const reverse = (arr) => {
+    const length = arr.length;
+    let middle = length / 2;
+    if (length % 2 !== 0) {
+        middle = (length - 1) / 2;
+    }
+    for (let i = 0; i < middle; i++) {
+        const temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+};
 export default reverse;
 // END
