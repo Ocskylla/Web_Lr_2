f6810b37acb7c9f387fdb4f345288f599b7e94c5
Ocskylla <BAAD410@yandex.ru>
2025-03-20 17:59:41 +0300

    Update 2-modification.js

--- solutions/2-modification.js
+++ solutions/2-modification.js
@@ -1,14 +1,15 @@
 // BEGIN
+export const swap = (arr) => {
+  if (!Array.isArray(arr)) {
+    return arr;
+  }
 
-export const swap =(arr)=>{
-    if (arr.length === 0) return arr;
-    if (arr.length === 1) return arr;
-    else {
-        let t = arr[0];
-        arr[0] = arr[arr.length-1];
-        arr[arr.length-1] = t;
-        return arr;
-    }
+  return arr.length < 2 ? arr : (() => {
+    const firstElement = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = firstElement;
+    return arr;
+  })();
 }
 export default swap;
 // END
